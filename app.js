cfcea6bd3df4bbdb029445b834c48aa8e5f5f91e
waqarabbas72 <sheraliate751@gmail.com>
2022-10-25 14:22:19 +0500

    todoApp

---
+++ app.js
@@ -0,0 +1,29 @@
+let input = document.getElementById("myInput")
+let todoList = document.getElementById("myList")
+const list = []
+
+const todoApp = {   
+    addFun(){
+        if(input.value){
+            list.push(input.value)
+            input.value = ""
+        }
+        this.myFun()
+    },
+    myFun(){
+        todoList.innerHTML = ""
+        list.forEach((value , index) => {
+            todoList.innerHTML += `<li>${value} <button onclick = "todoApp.delete(${index})" class = "del-btn">X</button></li>`
+    
+        })
+
+    },
+    clear(){
+        list.splice(0, list.length)
+        this.myFun()
+    },
+    delete(index){
+        list.splice(index ,1)
+        this.myFun()
+    }
+}
